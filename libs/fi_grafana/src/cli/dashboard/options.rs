8086b2708dab5cc8e38436889b7efd7bdace53cf
kevin.doering <kevin.doering@bcmsolutions.com>
2024-09-02 14:09:40 +0200

    add comments to the fields of the struct

--- libs/fi_grafana/src/cli/dashboard/options.rs
+++ libs/fi_grafana/src/cli/dashboard/options.rs
@@ -6,31 +6,31 @@ pub struct DashboardOptions {
     /// Use it to ref a dashboard by its instance id
     /// #[arg(short, long)]
     pub id: Option<u32>,
-    ///
+    /// Use it to ref a dashboard by its unique identifier
     #[arg(short, long)]
     pub uid: Option<String>,
-    ///
+    /// Use it to set the name for the dashboard itself
     #[arg(short, long)]
     pub name: String,
-    ///
+    /// Add search tags to the dashboard by specifying the new state
     #[arg(short, long)]
     pub tags: Vec<String>,
-    ///
+    /// Specify the timezone of the dashboard [default_timezone: 'browser']
     #[arg(short, long)]
     pub zone: String,
-    ///
+    /// Use it so set the schema version for the dashboard data
     #[arg(short, long)]
     pub schema_version: u16,
-    ///
+    /// Declare the refresh interval in seconds
     #[arg(short, long)]
     pub refresh_seconds: u8,
-    ///
+    /// Specify the folder in which the dashboards belong
     #[arg(short, long)]
     pub folder_uid: Option<String>,
-    ///
+    /// Set a message for the latest changes been made
     #[arg(short, long)]
     pub message: String,
-    ///
+    /// Enable overwriting of existing dashboards with the same name or uid
     #[arg(short, long)]
     pub overwrite: bool,
 }
