7fabd19443c246d9695fe5b947d847890b5e07f6
kevin-doering <kevin.doering.dt@gmail.com>
2024-09-25 13:28:14 +0200

    feat: add bash fns to find scoped dash types by name for panel annotations

---
+++ bin/add_anno_by_dash_name.sh
@@ -0,0 +1,47 @@
+#!/usr/bin/env bash
+
+# @author Kevin Doering <k.doering.dt@gmail.com>
+# @description Adds annotations to all panels within the named dashboard/folder scope by sending multiple requests to the grafana server utilizing cURL and jq.
+# @usage ./bin/add_annotation.sh bdvea4glj4fswf 1 "2024-09-04 08:00" "2024-09-04 08:30" tag comment
+
+# get absolute path to script and change context to script folder
+SCRIPT_PATH="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "${SCRIPT_PATH}" || exit
+
+# Source the functional library
+source ./grafana_lib.sh
+
+dotenv
+
+# Variables passed as arguments
+folderName="$1"
+dashboardName="$2"
+startDatetime="$3"
+endDatetime="$4"
+tags="$5"
+comment="$6"
+
+# Check env vars
+check_if_empty "$SERVICE_ACCOUNT_TOKEN" "SERVICE_ACCOUNT_TOKEN"
+check_if_empty "$GRAFANA_API_PATH" "GRAFANA_API_PATH"
+
+# Check each variable passed as argument
+check_if_empty "$folderName" "folderName"
+check_if_empty "$dashboardName" "dashboardName"
+# check_if_empty "$startDatetime" "startDatetime"
+# check_if_empty "$endDatetime" "endDatetime"
+# check_if_empty "$tags" "tags"
+# check_if_empty "$comment" "comment"
+
+# Validate startDatetime and endDatetime formats
+# validate_datetime_format "$startDatetime" "startDatetime"
+# validate_datetime_format "$endDatetime" "endDatetime"
+
+echo "Required variables are set!"
+# echo "Datetime values are correctly formatted: [format: %Y-%m-%d %H:%M]"
+echo "Sending annotation requests to the grafana server."
+
+# Create the annotation using the function from the library
+response=$(add_annotations_to_all_panel_within_the_specified_dash_type_scope "$folderName" "$dashboardName" "$startDatetime" "$endDatetime" "$tags" "$comment" "$GRAFANA_API_PATH" "$SERVICE_ACCOUNT_TOKEN")
+
+echo "$response"
--- bin/grafana_lib.sh
+++ bin/grafana_lib.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-function dotenv() {
+function dotenv {
   # Source the .env file to load environment variables
   DOT_ENV_PATH="../.env"
   if [ -f $DOT_ENV_PATH ]; then
@@ -13,7 +13,7 @@ function dotenv() {
 }
 
 # Function to check if a variable is empty
-function check_if_empty() {
+function check_if_empty {
   local var_value="$1"
   local var_name="$2"
   if [ -z "$var_value" ]; then
@@ -23,7 +23,7 @@ function check_if_empty() {
 }
 
 # Function to validate datetime format
-function validate_datetime_format() {
+function validate_datetime_format {
   local datetime="$1"
   local var_name="$2"
   if ! date -d "$datetime" "+%Y-%m-%d %H:%M" >/dev/null 2>&1; then
@@ -33,14 +33,14 @@ function validate_datetime_format() {
 }
 
 # Function to convert datetime to epoch milliseconds
-function datetime_to_epoch_ms() {
+function datetime_to_epoch_ms {
   local datetime="$1"
   time=$(date -d "$datetime" +%s%3N)
   echo "$time"
 }
 
 # Function to create an annotation in Grafana
-function create_grafana_annotation() {
+function create_grafana_annotation {
   local dashboardUid="$1"
   local panelId="$2"
   local startDatetime="$3"
@@ -73,8 +73,6 @@ function create_grafana_annotation() {
         "text": $text
       }')
 
-  echo "$json_payload"
-
   # Send the POST request to Grafana API
   response=$(curl -X POST -s "$grafana_url"annotations \
     -H "Accept: application/json" \
@@ -83,4 +81,75 @@ function create_grafana_annotation() {
     -d "$json_payload")
 
   echo "$response"
+}
+
+function add_annotations_to_all_panel_within_the_specified_dash_type_scope {
+  local folderName="$1"
+  local dashboardName="$2"
+  local startDatetime="$3"
+  local endDatetime="$4"
+  local tags="$5"
+  local text="$6"
+  local grafana_url="$7"
+  local api_key="$8"
+
+  local time
+  local timeEnd
+  local folderUids
+  local dashboardUids
+  local namedDashboardUids
+
+  # Transform datetime format
+  time=$(datetime_to_epoch_ms "$startDatetime")
+  timeEnd=$(datetime_to_epoch_ms "$endDatetime")
+
+  # Find uids of dash types matching the given names
+  folderUids=$(get_dash_type_uids_by_name "dash-folder" "$folderName" "$grafana_url" "$api_key")
+  dashboardUids=$(get_dash_type_uids_by_name "dash-db" "$dashboardName" "$grafana_url" "$api_key")
+  namedDashboardUids=$(get_named_dash_type_uids "dash-db" "$folderUids" "$dashboardUids" "$grafana_url" "$api_key")
+
+  # todo: get each dashboard by uid
+  # todo: filter panels by panel type timeseries
+  # todo: post anno for each panel
+
+  echo ""
+}
+
+function get_dash_type_uids_by_name {
+  local type="$1"
+  local query="$2"
+  local grafana_url="$3"
+  local api_key="$4"
+  local resource
+
+  resource="search?query=$query&type=$type"
+  response=$(curl -s -X GET "$grafana_url""$resource" \
+      -H "Accept: application/json" \
+      -H "Authorization: Bearer $api_key")
+  uids=$(echo "$response" | jq -r '.[].uid')
+
+  echo "$uids"
+}
+
+function get_named_dash_type_uids {
+  local type="$1"
+  local folderUids="$2"
+  local dashboardUids="$3"
+  local grafana_url="$4"
+  local api_key="$5"
+  local resource
+
+  resource="search?type=$type"
+  for fuid in $folderUids; do
+    resource+="&folderUIDs=$fuid"
+  done
+  for duid in $dashboardUids; do
+    resource+="&dashboardUIDs=$duid"
+  done
+  response=$(curl -s -X GET "$grafana_url""$resource" \
+      -H "Accept: application/json" \
+      -H "Authorization: Bearer $api_key")
+  uids=$(echo "$response" | jq -r '.[].uid')
+
+  echo "$uids"
 }
